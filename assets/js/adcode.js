8e8335542a3a2f33648d855593a2a4f6fd5d636f
Irawan Ria <irawanria85@gmail.com>
2026-04-22 23:47:02 +0700

    .

--- assets/js/adcode.js
+++ assets/js/adcode.js
@@ -1,16 +1,27 @@
 // kode iklan di tengah
 document.addEventListener("DOMContentLoaded", function () {
-    var adCode = `
-  <div class="ads-middle" style="margin:25px 0; text-align:center;">
-    {% include ad-middle.html %}
-  </div>
-  `;
 
-    var postBody = document.querySelector(".article-post");
-    if (!postBody) return;
-    var paragraphs = postBody.getElementsByTagName("p");
-    if (paragraphs.length > 3) {
-        var middleIndex = Math.floor(paragraphs.length / 2);
-        paragraphs[middleIndex].insertAdjacentHTML("afterend", adCode);
-    }
+  var adSource = document.getElementById("ad-middle-source");
+  if (!adSource) return;
+
+  var postBody = document.querySelector(".article-post");
+  if (!postBody) return;
+
+  var paragraphs = postBody.getElementsByTagName("p");
+
+  if (paragraphs.length > 3) {
+
+    var middleIndex = Math.floor(paragraphs.length / 2);
+
+    var adClone = adSource.cloneNode(true);
+    adClone.style.display = "block";
+    adClone.id = "";
+
+    paragraphs[middleIndex].insertAdjacentElement(
+      "afterend",
+      adClone
+    );
+
+  }
+
 });
